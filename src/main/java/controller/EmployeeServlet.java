09cba239575b07b7455c5b46c8998b8f05d7c5b6
lenovo <921790476@qq.com>
2016-09-23 00:00:06 +0800

    get load方法测试

--- src/main/java/controller/EmployeeServlet.java
+++ src/main/java/controller/EmployeeServlet.java
@@ -2,6 +2,9 @@ package controller;
 
 import dao.EmployeeTest;
 import domain.Employee;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -17,15 +20,26 @@ import java.io.IOException;
 public class EmployeeServlet extends HttpServlet {
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        EmployeeTest employeeTest = new EmployeeTest();
+       /* EmployeeTest employeeTest = new EmployeeTest();
         System.out.println("get");
-        employeeTest.get();
+        Employee employee = employeeTest.get();
+        System.out.println(employee.getId());
         System.out.println("get");
         System.out.println("=============");
         System.out.println("load");
-        Employee employee = employeeTest.load();
-        System.out.println(employee);
+        Employee employee1 = employeeTest.load();
+        System.out.println(employee1.getId());
+        System.out.println(employee1.getName());
         System.out.println("load");
-        req.getRequestDispatcher("/index.jsp").forward(req, resp);
+        req.getRequestDispatcher("/index.jsp").forward(req, resp);*/
+
+        Configuration configuration = new Configuration().configure();
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+        Session session = sessionFactory.openSession();
+        Employee employee = session.get(Employee.class, 10L);
+        //Employee employee1 = session.load(Employee.class, 10L);
+        System.out.println(employee);
+        session.close();
+        //System.out.println(employee1.getName());
     }
 }
